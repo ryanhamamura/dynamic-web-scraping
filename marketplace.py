b864c4b1b206567f661dfe557537b11c0c2db786
Ryan Hamamura <rhamamur@gmail.com>
2022-09-03 17:01:19 -1000

    Parsing data into a list of dicts

--- marketplace.py
+++ marketplace.py
@@ -9,6 +9,7 @@ import codecs
 import re
 from webdriver_manager.chrome import ChromeDriverManager
 from parsel import Selector
+from pprint import pp
 
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -21,9 +22,55 @@ def main():
     wait.until(EC.url_to_be(val))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sel = Selector(text=driver.page_source)
-    items = sel.xpath("//a[@role]//img[@alt]") 
+    # items = sel.xpath("//a[@role]//img[@alt]") 
+    items = sel.xpath("//a[@role]//div//span//div//span[@class][@dir]")
+    state = 'current_price'
+    parsed = [{},]
+    listing = {}
+    count = 0
     for item in items:
-        print(item.css('img::attr(alt)').get())
+        text = item.css('span::text').get()
+        # print(text)
+    for item in items:
+        text = item.css('span::text').get()
+        # print(f'[DEBUG] text is {text}')
+        if state == 'current_price':
+            # print(f'[DEBUG] state is {state}')
+            listing['current_price'] = text
+            if len(parsed) > count:
+                parsed.append({})
+            parsed[count]['current_price'] = text
+            # print(f'[DEBUG] {listing}')
+            state = 'previous_price'
+        elif state == 'previous_price':
+            # print(text[0])
+            # print(f'[DEBUG] state is {state}')
+            if text[0] == '$':
+                listing['previous_price'] = text
+                parsed[count]['previous_price'] = text
+                state = 'title'
+            else:
+                listing['title'] = text
+                parsed[count]['title'] = text
+                state = 'location'
+            # print(f'[DEBUG] {listing}')
+        elif state == 'title':
+            # print(f'[DEBUG] state is {state}')
+            listing['title'] = text
+            parsed[count]['title'] = text
+            # print(f'[DEBUG] {listing}')
+            state = 'location'
+        elif state == 'location':
+            # print(f'[DEBUG] state is {state}')
+            listing['location'] = text
+            parsed[count]['location'] = text
+            # print(f'[DEBUG] {listing}')
+            state = 'current_price'
+            listing.clear()
+            count += 1
+        else:
+            print("undefined state") 
+    pp(parsed)
     # if get_url == val:
         # page_source = driver.page_source
         # search = driver.find_elements(by=By.TAG_NAME, value='img')
